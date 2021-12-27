ec79b4041412f2a982caeab182e40788fbf0d64d
Frido Emans <verderster@gmail.com>
2021-12-27 21:46:56 +0100

    Laabr - improved logging (#8)
    use laabr instead of winston. Easier to set up and better integrated with hapi.

--- src/nsfwServer.js
+++ src/nsfwServer.js
@@ -6,18 +6,30 @@ const Boom = require('@hapi/boom');
 const axios = require('axios');
 const tf = require('@tensorflow/tfjs-node');
 const nsfw = require('nsfwjs');
-const logger = require('./logger');
+const laabr = require('laabr');
 const nsfwjsVersion = require('./nsfwVersion');
 
 if (process.env.NODE_ENV === 'production') {
   tf.enableProdMode();
-  logger.info('Tensorflow production mode enabled');
 }
 
 const nsfwServer = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 3000,
     host: process.env.HOST || 'localhost',
+  });
+
+  await server.register({
+    plugin: laabr,
+    options: {
+      hapiPino: {
+        logQueryParams: true,
+      },
+      formats: {
+        onPostStart: ':time :level :message on port :host[port]',
+        response: ':time :level :remoteAddress :method :url :get[queryParams] :status (:responseTime ms)',
+      },
+    },
   });
 
   // TODO: offline loading / self hosting
@@ -27,8 +39,6 @@ const nsfwServer = async () => {
     method: 'GET',
     path: '/',
     handler() {
-      logger.info('/ received');
-
       return 'This is the "Not suitable for work" server';
     },
   });
@@ -37,11 +47,10 @@ const nsfwServer = async () => {
     method: 'GET',
     path: '/classify',
     handler: async (request, h) => {
-      logger.info({ method: 'GET', message: request.url });
       const { url } = request.query;
 
-      if (url === undefined) {
-        return Boom.badRequest('no url specified');
+      if (!url) {
+        return Boom.badRequest('no url specified'); // 400
       }
 
       let axiosResponse;
@@ -51,12 +60,11 @@ const nsfwServer = async () => {
         });
       } catch (error) {
         const message = `Error fetching data for ${url} - got code ${error.toJSON().status}`;
-        logger.error({ code: 503, message });
         return Boom.serverUnavailable(message);
       }
 
       if (axiosResponse.status >= 400) {
-        return Boom.badRequest('bad url response');
+        return Boom.badRequest('bad url response'); // 400
       }
 
       try {
@@ -71,20 +79,17 @@ const nsfwServer = async () => {
           nsfwjsVersion,
         }).code(200);
       } catch (error) {
-        logger.error({ request: request.url, errorMessage: error.message });
         return Boom.unsupportedMediaType(error); // 415
       }
     },
   });
   await server.start();
 
-  logger.info({ message: `${process.env.NODE_ENV || 'development'} server running on port ${server.info.port}` });
-
   return server;
 };
 
 process.on('unhandledRejection', (reason, promise) => {
-  logger.error({ message: 'Unhandled rejection', reason, promise });
+  console.error({ message: 'Unhandled rejection', reason, promise });
   // Application specific logging, throwing an error, or other logic here
   process.exit(1);
 });
