e737cad6e9cd149881a98d17c2570757c869d4e8
Frido Emans <verderster@gmail.com>
2022-04-11 22:57:45 +0200

    allow usage of external ipfs daemon for creating the model CID

--- src/model.js
+++ src/model.js
@@ -1,19 +1,32 @@
-const { create, globSource } = require('ipfs');
+const { create, globSource } = this.process.env.IPFS_API_ADDRESS
+  ? require('ipfs')
+  : require('ipfs-http-client');
 const nsfw = require('nsfwjs');
 
 const modelsDirectory = `${process.env.PWD}/models`;
-// const modelFile = 'quant_nsfw_mobilenet';
-// const modelOptions = {};
+const availableModels = [
+  {
+    modelFile: 'quant_nsfw_mobilenet',
+    modelOptions: {},
+  },
+  {
+    modelFile: 'model',
+    modelOptions: { size: 299 },
+  },
+  {
+    modelFile: 'quant_mid',
+    modelOptions: { type: 'graph' },
+  },
+];
+const selectedModel = availableModels[2];
 
-// const modelFile = 'model';
-// const modelOptions = { size: 299 };
+const ipfsCreateOptions = this.process.env.IPFS_API_ADDRESS
+  ? this.process.env.IPFS_API_ADDRESS
+  : { offline: true, start: false };
 
-const modelFile = 'quant_mid';
-const modelOptions = { type: 'graph' };
-
-module.exports = async () => ({
+module.exports = async ({ modelFile, modelOptions } = selectedModel) => ({
   model: await nsfw.load(`file://${modelsDirectory}/${modelFile}/`, modelOptions),
-  modelCid: await create({ offline: true, start: false })
+  modelCid: await create(ipfsCreateOptions)
     .then(async (ipfs) => {
       let cid;
       // eslint-disable-next-line no-restricted-syntax
